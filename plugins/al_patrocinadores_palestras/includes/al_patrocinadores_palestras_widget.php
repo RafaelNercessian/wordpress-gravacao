a321ed5c644acb32f414d6af651fe76464b34920
unknown <rafanercessian@gmail.com>
2019-10-22 17:03:53 -0200

    Renderizando conteudo - Aula2.3

--- plugins/al_patrocinadores_palestras/includes/al_patrocinadores_palestras_widget.php
+++ plugins/al_patrocinadores_palestras/includes/al_patrocinadores_palestras_widget.php
@@ -23,19 +23,19 @@ class PatrocinadoresAlura extends WP_Widget
         <p>
             <input type="checkbox" id="<?= $this->get_field_id('caelum') ?>"
                    name="<?= $this->get_field_name('caelum') ?>"
-                   value="1">
+                   value="1" <?php checked('1', $instance['caelum']) ?> >
             <label for="<?= $this->get_field_id('caelum') ?>">Caelum</label>
         </p>
         <p>
             <input type="checkbox" id="<?= $this->get_field_id('casa_do_codigo')?>"
                    name="<?= $this->get_field_name('casa_do_codigo') ?>"
-                   value="1">
+                   value="1" <?php checked('1', $instance['casa_do_codigo']) ?>>
             <label for="<?= $this->get_field_id('casa_do_codigo')?>">Casa do Código</label>
         </p>
         <p>
             <input type="checkbox" id="<?= $this->get_field_id('hipsters')?>"
                    name="<?= $this->get_field_name('hipsters') ?>"
-                   value="1">
+                   value="1" <?php checked('1', $instance['hipsters']) ?>>
             <label for="<?= $this->get_field_id('hipsters')?>">Hipsters</label>
         </p>
         <?php
@@ -48,5 +48,25 @@ class PatrocinadoresAlura extends WP_Widget
         $instance['casa_do_codigo'] = !empty($new_instance['casa_do_codigo']) ? strip_tags($new_instance['casa_do_codigo']) : '';
         $instance['hipsters'] = !empty($new_instance['hipsters']) ? strip_tags($new_instance['hipsters']) : '';
         return $instance;
+    }
+
+    public function widget($args, $instance)
+    {
+        ?>
+            <section class="patrocinadores-principais">
+                <h3 class="titulo-patrocinadores">Patrocinadores</h3>
+                <ul class="lista-patrocinadores">
+                    <?php if(!empty($instance['caelum'])): ?>
+                        <li><img src="<?= plugin_dir_url(__FILE__) .'../images/caelum.svg' ?>"></li>
+                    <?php endif; ?>
+                    <?php if(!empty($instance['casa_do_codigo'])): ?>
+                        <li><img src="<?= plugin_dir_url(__FILE__) .'../images/cdc.svg' ?>"></li>
+                    <?php endif; ?>
+                    <?php if(!empty($instance['hipsters'])): ?>
+                        <li><img src="<?= plugin_dir_url(__FILE__) .'../images/hipsters.svg' ?>"></li>
+                    <?php endif; ?>
+                </ul>
+            </section>
+        <?php
     }
 }

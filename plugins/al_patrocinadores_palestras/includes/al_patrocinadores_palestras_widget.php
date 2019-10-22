e178dba318027e1bd0a45b3676f675955b22c696
unknown <rafanercessian@gmail.com>
2019-10-22 14:16:22 -0200

    Criando formulario Widget Aula1.5

---
+++ plugins/al_patrocinadores_palestras/includes/al_patrocinadores_palestras_widget.php
@@ -0,0 +1,38 @@
+<?php
+
+class PatrocinadoresAlura extends WP_Widget
+{
+
+    public function __construct()
+    {
+        parent::__construct(
+            'al_patrocinadores_palestras_widget',
+            'Patrocinadores Palestras',
+            array('description' => 'Selecione os patrocinadores da palestra')
+        );
+    }
+
+    public function form($instance)
+    {
+        ?>
+        <p>
+            <input type="checkbox" id="<?= $this->get_field_id('caelum') ?>"
+                   name="<?= $this->get_field_name('caelum') ?>"
+                   value="1">
+            <label for="<?= $this->get_field_id('caelum') ?>">Caelum</label>
+        </p>
+        <p>
+            <input type="checkbox" id="<?= $this->get_field_id('casa_do_codigo')?>"
+                   name="<?= $this->get_field_name('casa_do_codigo') ?>"
+                   value="1">
+            <label for="<?= $this->get_field_id('casa_do_codigo')?>">Casa do Código</label>
+        </p>
+        <p>
+            <input type="checkbox" id="<?= $this->get_field_id('hipsters')?>"
+                   name="<?= $this->get_field_name('hipsters') ?>"
+                   value="1">
+            <label for="<?= $this->get_field_id('hipsters')?>">Hipsters</label>
+        </p>
+        <?php
+    }
+}

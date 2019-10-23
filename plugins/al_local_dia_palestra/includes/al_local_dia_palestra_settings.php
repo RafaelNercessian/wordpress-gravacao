facdf8e9127d440698fcd056df96797c916ee9e4
unknown <rafanercessian@gmail.com>
2019-10-23 12:12:27 -0200

    Aula 3.2

--- plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
+++ plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
@@ -1,7 +1,8 @@
 <?php
 
-add_action('admin_menu','al_local_dia_palestra_menu');
-function al_local_dia_palestra_menu(){
+add_action('admin_menu', 'al_local_dia_palestra_menu');
+function al_local_dia_palestra_menu()
+{
     add_menu_page(
         'Local Palestra',
         'Local Palestra',
@@ -10,5 +11,30 @@ function al_local_dia_palestra_menu(){
         'al_local_dia_palestra_menu_pagina',
         'dashicons-location-alt',
         -1
+    );
+}
+
+function al_local_dia_palestra_menu_pagina()
+{
+    ?>
+    <div>
+        <h1>Local Palestras</h1>
+        <form method="post" action="options.php">
+            <?php
+                do_settings_sections('local-palestra');
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
+add_action('admin_menu', 'al_local_dia_palestra_secao');
+function al_local_dia_palestra_secao()
+{
+    add_settings_section(
+        'al_local_dia_palestra_secao',
+        'Configurações do local da palestra',
+        'al_local_dia_palestra_campos_secao_detalhes',
+        'local-palestra'
     );
 }

c087f0ecfcb7a7ebe7e5978268aaa229ace58526
unknown <rafanercessian@gmail.com>
2019-10-23 14:43:09 -0200

    Aula 3.5

--- plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
+++ plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
@@ -21,6 +21,7 @@ function al_local_dia_palestra_menu_pagina()
         <h1>Local Palestras</h1>
         <form method="post" action="options.php">
             <?php
+            settings_errors();
             do_settings_sections('local-palestra');
             settings_fields('al_local_dia_palestra_settings');
             submit_button();
@@ -52,7 +53,8 @@ function al_local_dia_palestra_secao()
 
     register_setting(
         'al_local_dia_palestra_settings',
-        'al_local_dia_palestra_endereco'
+        'al_local_dia_palestra_endereco',
+        'verifica_endereco'
     );
 
     //Cidade
@@ -66,7 +68,8 @@ function al_local_dia_palestra_secao()
 
     register_setting(
         'al_local_dia_palestra_settings',
-        'al_local_dia_palestra_cidade'
+        'al_local_dia_palestra_cidade',
+        'verifica_cidade'
     );
 
     //Data
@@ -80,7 +83,8 @@ function al_local_dia_palestra_secao()
 
     register_setting(
         'al_local_dia_palestra_settings',
-        'al_local_dia_palestra_data'
+        'al_local_dia_palestra_data',
+        'verifica_data'
     );
 }
 
@@ -97,7 +101,7 @@ function al_local_dia_palestra_endereco()
 {
     ?>
     <input type="text" id="al_local_dia_palestra_endereco"
-           name="al_local_dia_palestra_endereco" required>
+           name="al_local_dia_palestra_endereco" value="<?= esc_attr(get_option('al_local_dia_palestra_endereco')) ?>" required>
     <?php
 }
 
@@ -106,7 +110,7 @@ function al_local_dia_palestra_cidade()
 {
     ?>
     <input type="text" id="al_local_dia_palestra_cidade"
-           name="al_local_dia_palestra_cidade" required>
+           name="al_local_dia_palestra_cidade" value="<?= esc_attr(get_option('al_local_dia_palestra_cidade')) ?>"required>
     <?php
 }
 
@@ -115,6 +119,52 @@ function al_local_dia_palestra_data()
 {
     ?>
     <input type="date" id="al_local_dia_palestra_data"
-           name="al_local_dia_palestra_data" required>
+           name="al_local_dia_palestra_data" value="<?= esc_attr(get_option('al_local_dia_palestra_data'))?>" required>
     <?php
+}
+
+/*
+ * Configuração de callback de verificação dos campos
+ */
+
+//Enedereço
+function verifica_endereco($endereco){
+    if(empty($endereco)){
+        $endereco = get_option('al_local_dia_palestra_endereco');
+        add_settings_error(
+            'al_local_dia_palestra_mensagem_erro',
+            'al_local_dia_palestra_erro_endereco',
+            'O campo endereço não pode ser vazio!',
+            'error'
+        );
+    }
+    return $endereco;
+}
+
+//Cidade
+function verifica_cidade($cidade){
+    if(empty($cidade)){
+        $cidade = get_option('al_local_dia_palestra_cidade');
+        add_settings_error(
+            'al_local_dia_palestra_mensagem_erro',
+            'al_local_dia_palestra_erro_cidade',
+            'O campo cidade não pode ser vazio!',
+            'error'
+        );
+    }
+    return $cidade;
+}
+
+//Data
+function verifica_data($data){
+    if(empty($data)){
+        $data = get_option('al_local_dia_palestra_data');
+        add_settings_error(
+            'al_local_dia_palestra_mensagem_erro',
+            'al_local_dia_palestra_erro_data',
+            'O campo data não pode ser vazio!',
+            'error'
+        );
+    }
+    return $data;
 }

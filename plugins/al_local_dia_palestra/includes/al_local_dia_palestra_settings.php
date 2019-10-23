069ed25765025108529c6a3ad5e85000d0e6df12
unknown <rafanercessian@gmail.com>
2019-10-23 12:53:11 -0200

    Aula 3.3

--- plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
+++ plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
@@ -21,7 +21,8 @@ function al_local_dia_palestra_menu_pagina()
         <h1>Local Palestras</h1>
         <form method="post" action="options.php">
             <?php
-                do_settings_sections('local-palestra');
+            do_settings_sections('local-palestra');
+            settings_fields('al_local_dia_palestra_settings');
             ?>
         </form>
     </div>
@@ -37,4 +38,34 @@ function al_local_dia_palestra_secao()
         'al_local_dia_palestra_campos_secao_detalhes',
         'local-palestra'
     );
+
+    add_settings_field(
+        'al_local_dia_palestra_endereco',
+        'Endereço',
+        'al_local_dia_palestra_endereco',
+        'local-palestra',
+        'al_local_dia_palestra_secao'
+    );
+
+    register_setting(
+        'al_local_dia_palestra_settings',
+        'al_local_dia_palestra_endereco'
+    );
+}
+
+//Função callback seção
+function al_local_dia_palestra_campos_secao_detalhes()
+{
+    ?>
+    <p>Insira os dados do endereço, cidade e data da próxima palestra da Alura</p>
+    <?php
+}
+
+//Função callback endereço
+function al_local_dia_palestra_endereco()
+{
+    ?>
+    <input type="text" id="al_local_dia_palestra_endereco"
+           name="al_local_dia_palestra_endereco" required>
+    <?php
 }

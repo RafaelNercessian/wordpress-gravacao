d1f91bfdf132eda61da46622a791edf24bccea3e
unknown <rafanercessian@gmail.com>
2019-10-23 11:22:56 -0200

    Aula3.1 regravado

--- plugins/al_local_dia_palestra/al_local_dia_palestra.php
+++ plugins/al_local_dia_palestra/al_local_dia_palestra.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Plugin Name: Cadastro local e data da palestra
- * Description: Plugin para cadastrar local e data da próxima palestra da Alura
+ * Plugin Name: Cadastrar local e data da palestra
+ * Description: Plugin para cadastrar local e data da próxima palestra realizada pela Alura
  * Version: 1.0.0
  * Author: Rafael Nercessian
  */
@@ -9,3 +9,5 @@
 if(!defined('ABSPATH')){
     die;
 }
+
+require_once plugin_dir_path(__FILE__) . '/includes/al_local_dia_palestra_settings.php';

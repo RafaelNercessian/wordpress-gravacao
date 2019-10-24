fd7b1f1eaefe32f49abdfd2d8d23f12ae0ee6aa0
unknown <rafanercessian@gmail.com>
2019-10-24 14:08:19 -0200

    Aula5.3

---
+++ plugins/al_temperatura_palestra/al_temperatura_palestra.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Temperatura Palestra
+ * Description: Plugin para mostrar a temperatura no local da palestra
+ * Version: 1.0.0
+ * Author: Rafael Nercessian
+ */
+
+if(!defined('ABSPATH')){
+    die;
+}
---
+++ plugins/al_temperatura_palestra/includes/al_temperatura_palestra_widget.php
@@ -0,0 +1,30 @@
+<?php
+
+class TemperaturaPalestra extends WP_Widget{
+
+    public function __construct()
+    {
+        parent::__construct(
+            'al_temperatura_palestra_widget',
+            'Temperatura Palestra',
+            array('description' => 'Mostrar a temperatura no local da palestra')
+        );
+    }
+
+    public function widget($args, $instance)
+    {
+        $cidade = urlencode(get_option('al_local_dia_palestra_cidade'));
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "https://api.openweathermap.org/data/2.5/weather?q=". $cidade .",br&appid=".OPENWEATHER_API_KEY);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $resultado = curl_exec($curl);
+        $resultadoArray = json_decode($resultado, true);
+        ?>
+        <section class="container-temperatura">
+            <p class="cidade-temperatura"><?= get_option('al_local_dia_palestra_cidade') ?></p>
+            <p class="temperatura"><?= round($resultadoArray['main']['temp'] -273.15) ?></p>
+        </section>
+        <?php
+    }
+
+}
